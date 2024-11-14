7cc9b83b1f46128e7ece7640e76f299da155c17c
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-11-14 19:36:06 +0530

    fix : file read error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import { FilesystemItem } from "@/components/filesystem-item";
 import { Node } from "@/types";
 import fs from "node:fs";
-import path from "node:path";
 
 export default function Home() {
   const readFolder = (dirPath: string): Node[] => {
     // Read all files and directories in the given directory
-    const items = fs.readdirSync(dirPath, { withFileTypes: true });
+    const items = fs.readdirSync(`${process.cwd()}/${dirPath}`, {
+      withFileTypes: true,
+    });
 
     return items.map((item) => {
-      const fullPath = process.cwd() + dirPath + item.name;
+      const fullPath = dirPath + "/" + item.name;
       if (item.isDirectory()) {
         return {
           name: item.name,
@@ -27,7 +28,7 @@ export default function Home() {
     });
   };
 
-  const fileTree = readFolder("./src/folders");
+  const fileTree = readFolder("src/folders");
 
   return (
     <main className="p-4 sm:p-8 font-[family-name:var(--font-geist-sans)]">
