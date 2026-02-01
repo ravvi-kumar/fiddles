de0dd88aac459ce7447c46bad4d515a20818f2f1
Ravi Kumar <53389976+ravvi-kumar@users.noreply.github.com>
2026-02-01 11:25:51 +0530

    Add new greeting URL to welcome function

--- src/folders/welcome.ts
+++ src/folders/welcome.ts
@@ -2,5 +2,6 @@ function greet() {
   const name: string = "Hello World";
   console.log(name);
   console.log("https://picuki.site");
+  console.log("https://pimpbunny.com");
   console.log("Welcome to my fiddle!");
 }
